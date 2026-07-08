f742a867e631f90d8223d5185af11b564508c18e
Cihan C. Günhan <cihancgunhan@gmail.com>
2026-07-08 15:11:18 +0300

    feat: Add film detail page with showtimes

--- src/app/components/film-detail/film-detail.ts
+++ src/app/components/film-detail/film-detail.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { SlicePipe } from '@angular/common';
+import { FilmService } from '../../services/film';
 
 @Component({
   selector: 'app-film-detail',
-  imports: [],
+  imports: [RouterLink, SlicePipe],
   templateUrl: './film-detail.html',
-  styleUrl: './film-detail.css',
+  styleUrl: './film-detail.css'
 })
-export class FilmDetail {}
+export class FilmDetail implements OnInit {
+
+  film: any = null;
+  seanslar: any[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private filmService: FilmService
+  ) {}
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.loadFilm(id);
+    this.loadSeanslar(id);
+  }
+
+  loadFilm(id: number) {
+    this.filmService.getFilmById(id).subscribe({
+      next: (film) => this.film = film
+    });
+  }
+
+  loadSeanslar(id: number) {
+    this.filmService.getSeanslarByFilm(id).subscribe({
+      next: (seanslar) => this.seanslar = seanslar
+    });
+  }
+}
